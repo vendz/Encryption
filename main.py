d3c09738f140c8889b28dc5afb33440b7647681a
vandit <vasavandit@gmail.com>
2020-12-13 10:42:12 +0530

    adding file support

--- main.py
+++ main.py
@@ -19,19 +19,46 @@ kdf = PBKDF2HMAC (
 key = base64.urlsafe_b64encode(kdf.derive(password))  #can only use KDF(key derivation function) once
 f = Fernet(key)
 
-ask_input = input('do you want to encrypt or decrypt (E or D): ')
-if ask_input.upper() == 'E':
-    message = input('enter a message you want to encrypt: ')
-    byte_message = bytes(message, 'utf-8')     #converting user input to byte array
-    encrypted_message = f.encrypt(byte_message)  
-    print(encrypted_message)
+ef_df = input("do you want to work with files or text (F or T): ")   #asking client if he/she wan't to work with file or message 
+if ef_df.upper() == 'F':
+    ask_input_f = input('do you want to encrypt or decrypt the file (E or D): ')
+    if ask_input_f.upper() == 'E':
+        filename = input("copy the file you want to encrypt to this folder and enter it's name: ")
+        with open(filename, 'rb') as s:    #we will open a file with read byte
+            e_file = s.read()
+        encrypted_file = f.encrypt(e_file)
+        with open('your encrypted file.pages', 'wb') as es:    #we will create new file with encrypted data in it with write byte
+            es.write(encrypted_file)
 
-elif ask_input.upper() == 'D':
-    en_message = input('enter the encrypted message: ') #copy the encrypted key and paste it here(copy without the b' ')
-    byte_en_message = bytes(en_message, 'utf-8')  #converting the message to byte array
-    decrypt_message = f.decrypt(byte_en_message)
-    original_message = decrypt_message.decode()  #converting byte array to string
-    print(original_message)
+    elif ask_input_f.upper() == 'D':
+        d_filename = input("copy the file you want to encrypt to this folder and enter it's name: ")
+        with open(d_filename, 'rb') as df:
+            encrypted_data = df.read()
+        decrypted_file = f.decrypt(encrypted_data)
+        with open('your decrypted file.pages', 'wb'):
+            df.write()
+
+    else :
+        print("please enter a valid character.")
+
+elif ef_df.upper() == 'T':
+
+    ask_input = input('do you want to encrypt or decrypt (E or D): ')
+    if ask_input.upper() == 'E':
+        message = input('enter a message you want to encrypt: ')
+        byte_message = bytes(message, 'utf-8')     #converting user input to byte array
+        encrypted_message = f.encrypt(byte_message)  
+        print(encrypted_message)
+
+    elif ask_input.upper() == 'D':
+        en_message = input('enter the encrypted message: ') #copy the encrypted key and paste it here(copy without the b' ')
+        byte_en_message = bytes(en_message, 'utf-8')  #converting the message to byte array
+        decrypt_message = f.decrypt(byte_en_message)
+        original_message = decrypt_message.decode()  #converting byte array to string
+        print(original_message)
+
+    else :
+        print("please enter a valid character.")
 
 else :
-    print("please enter a valid character.")
+        print("please enter a valid character.")
